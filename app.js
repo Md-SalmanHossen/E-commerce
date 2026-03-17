86ff0e43237e38e200017b1e73fae8934a35c1fe
SalmanHossen <hossainsalman873@gmail.com>
2026-03-17 19:17:17 +0600

    setup app.js

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-import express from 'express';
-import rateLimiter from 'express-rate-limit';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize'
-import hpp from 'hpp';
-import cors from 'cors';
-import path from 'path'
-import cookieParser from 'cookie-parser';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import rateLimiter from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cors from "cors";
+import path from "path";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+import { connectDb } from "./src/configs/connectDB.config";
 
 dotenv.config();
+connectDb();
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5173"],
+    credentials: true,
+  }),
+);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https:", "data:"],
+    },
+  }),
+);
+
+app.use(mongoSanitize);
+app.use(hpp());
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb'}));
+
+const limiter=rateLimiter({
+   windowMs:15*60*1000,
+   max:3000
+});
+app.use(limiter);
+
+
+export default app;
+
 
